ab8f1d4ff09e53002eb22c6980aaa78ef0e29dda
Aabhaa04 <kshatriyabha@gmail.com>
2025-09-17 01:00:41 +0530

    Final working project

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { LandingComponent } from './landing/landing.component';
-
-export const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: 'home', component: Home },
-];
